a9fc5632fee9e196370c1e8750bcb1093a13234a
Matt Kramer <matthew123003@gmail.com>
2024-04-17 14:18:44 -0400

    Entites and annotations done

--- src/main/java/entities/Artist.java
+++ src/main/java/entities/Artist.java
@@ -1,12 +1,14 @@
 package entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Artist")
 public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String artist;
-    private Integer numOfMembers;
+    private Integer num_Of_Members;
 }
--- src/main/java/entities/Songs.java
+++ src/main/java/entities/Songs.java
@@ -1,15 +1,18 @@
 package entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Songs")
 public class Songs {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String genre;
     private Integer year;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist_id")
     private String artist;
 }
