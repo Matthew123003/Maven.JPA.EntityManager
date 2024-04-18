c489d9a80b092a11552a10b966a97f1a4135aa0d
Matt Kramer <matthew123003@gmail.com>
2024-04-18 07:32:27 -0400

    Create Finished

--- src/main/java/services/ArtistService.java
+++ src/main/java/services/ArtistService.java
@@ -1,5 +1,7 @@
 package services;
 
+import entities.Artist;
+import entities.Songs;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -8,6 +10,22 @@ public class ArtistService implements Services{
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    public void createArtist(Artist artist) {
+        // Begin a transaction
+        entityManager.getTransaction().begin();
+        try {
+            // Persist the Songs entity
+            entityManager.persist(artist);
+            // Commit the transaction
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            // Rollback the transaction in case of an error
+            entityManager.getTransaction().rollback();
+            // Optionally, handle the exception
+            throw e;
+        }
+    }
 
 
     @Override
